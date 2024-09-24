652603dbd0aa6076b3137ab0a7d183353fb79306
Maliket <beha.bekki.81.29.9@gmail.com>
2024-09-24 20:11:57 +0700

    update js

--- src/js/jsConfig/projects.js
+++ src/js/jsConfig/projects.js
@@ -2,15 +2,15 @@ const $projectsItem = document.querySelectorAll('.projects__item');
 const $projectsItemWrap = document.querySelectorAll('.projects__item-wrap');
 
 const imagesFull = [
-     '/images/my-projects/project-1.png',
-     '/images/my-projects/project-2.png',
-     '/images/my-projects/project-3.png',
+     './images/my-projects/project-1.png',
+     './images/my-projects/project-2.png',
+     './images/my-projects/project-3.png',
 ];
 
 const imagesMobile = [
-     '/images/my-projects/project-1-320.png',
-     '/images/my-projects/project-2-320.png',
-     '/images/my-projects/project-3-320.png',
+     './images/my-projects/project-1-320.png',
+     './images/my-projects/project-2-320.png',
+     './images/my-projects/project-3-320.png',
 ];
 
 $projectsItem.forEach((item, index) => {
