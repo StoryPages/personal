a7f6e88667314d71b27173f1f03ecbf4c297524b
Maliket <beha.bekki.81.29.9@gmail.com>
2024-09-25 00:57:45 +0700

    update form.js

--- src/js/jsConfig/form.js
+++ src/js/jsConfig/form.js
@@ -1,21 +1,25 @@
-// Axios
 import axios from 'axios';
 
 const $alert = document.querySelector('.alert');
 const $alertContainer = document.querySelector('.alert__container');
 const $formButton = document.querySelector('.form__button');
 const $alertBtn = document.querySelector('.alert__btn');
-const $body = document.querySelector('.body');
-
+const messageElementId = ['error-message-name', 'error-message-url', 'error-message-text'];
 // Form id
-const FORM = document.getElementById('form');
 const NAME = document.getElementById('name');
 const URL = document.getElementById('url');
 const TEXT = document.getElementById('text');
+const FORM = document.getElementById('form');
 
 // Array id inputs
 const arrayIdFormInputs = [NAME, URL, TEXT];
+// Message elements
+const messageElements = messageElementId.map((id) => document.getElementById(id));
+messageElements.forEach((errorMessage) => {
+     errorMessage.classList.add('none');
+});
 
+// Post data
 const TOKEN = '8004900723:AAF357Hf41r_UWSMNuswTiG37we1ejov8Hc';
 const CHAT_ID = '-1002350522727';
 const URI_API = `https://api.telegram.org/bot${TOKEN}/sendMessage`;
@@ -30,56 +34,87 @@ function sendMessage() {
           chat_id: CHAT_ID,
           parse_mode: 'HTML',
           text: message,
-     })
-          .then((res) => {
-               NAME.value = '';
-               URL.value = '';
-               TEXT.value = '';
-          })
-          .catch((err) => {
-               console.log(err);
-          })
-          .finally(() => {
-               console.log('Data sent successfully');
-          });
+     });
 }
 
-$formButton.onclick = (event) => {
+// Validation form
+FORM.addEventListener('submit', function (event) {
      event.preventDefault();
 
-     // Function alert show
-     const alertShow = () => {
-          $alert.classList.add('show');
-          $alertContainer.classList.add('show');
-     };
+     const isNameValid = validationInputName();
+     const isUrlValid = validationInputUrl();
+     const isTextValid = validationInputText();
 
-     // Function add shake animation
-     let allFilled = true;
-
-     arrayIdFormInputs.forEach((input) => {
-          if (input.value.trim()) {
-               input.classList.remove('shake-animation');
-          } else {
-               input.classList.add('shake-animation');
-               setTimeout(() => {
-                    input.classList.remove('shake-animation');
-               }, 300);
-               allFilled = false;
-          }
-     });
-
-     if (allFilled) {
-          sendMessage();
-          arrayIdFormInputs.forEach((input) => {
-               input.value = '';
+     if (isNameValid && isUrlValid && isTextValid) {
+          arrayIdFormInputs.forEach((inputs) => {
+               inputs.value = '';
           });
-          alertShow();
+          sendMessage();
      }
-};
-
-function removeClassShow() {
-     $alert.classList.remove('show');
-     $alertContainer.classList.remove('show');
+});
+function validationInputName() {
+     const isText = /^[a-zA-Za-яА-ЯёЁ\s]+$/;
+     if (NAME.value === '') {
+          messageElements[0].classList.remove('none');
+          messageElements[0].textContent = 'Enter your name';
+          NAME.classList.add('border-red');
+          setTimeout(() => {
+               NAME.classList.remove('shake-animation');
+          }, 100);
+          return false;
+     } else if (!isText.test(NAME.value)) {
+          messageElements[0].classList.remove('none');
+          messageElements[0].textContent = 'Only letters are available';
+          NAME.classList.add('shake-animation');
+          return false;
+     } else {
+          messageElements[0].classList.add('none');
+          NAME.classList.remove('border-red');
+          setTimeout(() => {
+               NAME.classList.remove('shake-animation');
+          }, 100);
+          return true;
+     }
 }
 
-$alertBtn.onclick = () => removeClassShow();
+function validationInputUrl() {
+     const urlPattern = /^(https?:\/\/)?([a-z0-9-]+\.)+[a-z]{2,6}(\/[^\s]*)?$/i;
+     if (URL.value === '') {
+          messageElements[1].classList.remove('none');
+          messageElements[1].textContent = 'Enter the URL';
+          URL.classList.add('border-red');
+          setTimeout(() => {
+               URL.classList.remove('shake-animation');
+          }, 100);
+          return false;
+     } else if (!urlPattern.test(URL.value)) {
+          messageElements[1].classList.remove('none');
+          messageElements[1].textContent = 'Please enter a valid URL';
+          URL.classList.add('shake-animation');
+          return false;
+     } else {
+          messageElements[1].classList.add('none');
+          URL.classList.remove('border-red');
+          setTimeout(() => {
+               URL.classList.remove('shake-animation');
+          }, 100);
+          return true;
+     }
+}
+
+function validationInputText() {
+     if (TEXT.value.trim() === '') {
+          messageElements[2].classList.remove('none');
+          messageElements[2].textContent = 'Enter text';
+          TEXT.classList.add('border-red');
+          return false;
+     } else {
+          messageElements[2].classList.add('none');
+          TEXT.classList.remove('border-red');
+          return true;
+     }
+}
+
+NAME.addEventListener('input', validationInputName);
+URL.addEventListener('input', validationInputUrl);
+TEXT.addEventListener('input', validationInputText);
--- src/js/main.js
+++ src/js/main.js
@@ -3,4 +3,3 @@ import './jsConfig/projects';
 import './jsConfig/testimonial';
 import './jsConfig/mobile-nav';
 import './jsConfig/form';
-import './jsConfig/validation-form';
